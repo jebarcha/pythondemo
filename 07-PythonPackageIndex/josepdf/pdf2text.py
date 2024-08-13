21ab98890ab433d3e000b8933d1e83b653eb2d28
jebarcha <jebarcha@hotmail.com>
2024-08-13 09:47:54 -0600

    package index demo

---
+++ 07-PythonPackageIndex/josepdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" this module provides functions to convert a PDF to text """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text. """
+
+    def convert(self, path):
+        """ 
+        Convert the given PDF to text. 
+
+        Parameters:
+        path (str): The path to a PDF file.
+
+        Returns:
+        str: The content of the PDF as text.
+        """
+        print("pdf2text")
