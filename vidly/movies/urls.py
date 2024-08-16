afb6a6e06562399711d583ebedfc5524fe1c8470
jebarcha <jebarcha@hotmail.com>
2024-08-16 09:53:03 -0600

    views completed

--- vidly/movies/urls.py
+++ vidly/movies/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 # movies/  this is the root of this app
 # movies/1/details
+app_name = "movies"
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    #output = ', '.join([m.title for m in movies])
+    #return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
+
+    # # SELECT * FROM movies_movie
+    # Movie.objects.filter(release_year=1964)
+    # # SELECT * FROM movies_movie WHERE release_year=1964
+    # Movie.objects.get(id=1)
+    # return HttpResponse("Hello World")
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', { 'movie': movie })
+    # try:
+    #     movie = Movie.objects.get(pk=movie_id)
+    #     return render(request, 'movies/detail.html', { 'movie': movie })
+    # except Movie.DoesNotExist:
+    #     raise Http404()
